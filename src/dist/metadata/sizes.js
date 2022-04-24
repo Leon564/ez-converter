0bcbeeef020b3fa46517e2c29c97e8109e947bc2
Leon564 <nleon564.nl@gmail.com>
2022-04-24 16:44:27 -0600

    feat(metadata): added sizes export

---
+++ src/dist/metadata/sizes.js
@@ -0,0 +1,9 @@
+module.exports = {
+  ORIGINAL: "original",
+  P600XAUTO: "600",
+  P500XAUTO: "500",
+  P480XAUTO: "480",
+  P320XAUTO: "320",
+  AUTOX480: "480p",
+  AUTOX320: "320p",
+};
--- src/index.js
+++ src/index.js
@@ -1,3 +1,3 @@
 exports.Convert = require("./dist/index");
 exports.Formats = require("./dist/metadata/formats");
-
+exports.Sizes = require("./dist/metadata/sizes");
